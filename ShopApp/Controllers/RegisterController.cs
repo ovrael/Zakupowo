d1b5c01260dfe33c5c3b1401a3a70b419d543b28
ovrael <ovrael@gmail.com>
2020-10-19 20:14:10 +0200

    Added database connection for Code-First method

--- ShopApp/Controllers/RegisterController.cs
+++ ShopApp/Controllers/RegisterController.cs
@@ -1,125 +1,127 @@
-﻿using ShopApp.Models;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿//using ShopApp.Models;
+//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Linq;
+//using System.Web;
+//using System.Web.Mvc;
 
-namespace ShopApp.Controllers
-{
-    public class RegisterController : Controller
-    {
-        // GET: Register
-        public ActionResult Index()
-        {
-            return View();
-        }
+// FIXME
 
-        // GET: Register/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+//namespace ShopApp.Controllers
+//{
+//    public class RegisterController : Controller
+//    {
+//        // GET: Register
+//        public ActionResult Index()
+//        {
+//            return View();
+//        }
 
-        // GET: Register/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+//        // GET: Register/Details/5
+//        public ActionResult Details(int id)
+//        {
+//            return View();
+//        }
 
-        // POST: Register/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
+//        // GET: Register/Create
+//        public ActionResult Create()
+//        {
+//            return View();
+//        }
 
-                //Adding new User to database
-                ViewData["FirstName"] = collection[1];
-                ViewData["LastName"] = collection[2];
-                ViewData["BirthDate"] = collection[3];
-                ViewData["Login"] = collection[4];
-                ViewData["EncryptedPassword"] = collection[5];
-                ViewData["Email"] = collection[6];
-                ViewData["Phone"] = collection[7];
-                ViewData["Country"] = collection[8];
-                ViewData["City"] = collection[9];
+//        // POST: Register/Create
+//        [HttpPost]
+//        public ActionResult Create(FormCollection collection)
+//        {
+//            try
+//            {
+//                // TODO: Add insert logic here
 
-                var context = new ShopEntities();
+//                //Adding new User to database
+//                ViewData["FirstName"] = collection[1];
+//                ViewData["LastName"] = collection[2];
+//                ViewData["BirthDate"] = collection[3];
+//                ViewData["Login"] = collection[4];
+//                ViewData["EncryptedPassword"] = collection[5];
+//                ViewData["Email"] = collection[6];
+//                ViewData["Phone"] = collection[7];
+//                ViewData["Country"] = collection[8];
+//                ViewData["City"] = collection[9];
 
-                DateTime.TryParse((string)ViewData["BirthDate"], out DateTime birthDate);
+//                //var context = new ShopEntities();
 
-                //Creating new user, UserID and CreationDate will be declared automatically
-                var user = new Users()
-                {
-                    FirstName = (string)ViewData["FirstName"],
-                    LastName = (string)ViewData["LastName"],
-                    BirthDate = birthDate,
-                    Login = (string)ViewData["Login"],
-                    EncryptedPassword = Cryptographing.Encrypt((string)ViewData["EncryptedPassword"]),
-                    Email = (string)ViewData["Email"],
-                    Phone = (string)ViewData["Phone"],
-                    Country = (string)ViewData["Country"],
-                    City = (string)ViewData["City"],
-                };
+//                DateTime.TryParse((string)ViewData["BirthDate"], out DateTime birthDate);
 
-                context.Users.Add(user);
-                context.SaveChanges();
+//                //Creating new user, UserID and CreationDate will be declared automatically
+//                var user = new Users()
+//                {
+//                    FirstName = (string)ViewData["FirstName"],
+//                    LastName = (string)ViewData["LastName"],
+//                    BirthDate = birthDate,
+//                    Login = (string)ViewData["Login"],
+//                    EncryptedPassword = Cryptographing.Encrypt((string)ViewData["EncryptedPassword"]),
+//                    Email = (string)ViewData["Email"],
+//                    Phone = (string)ViewData["Phone"],
+//                    Country = (string)ViewData["Country"],
+//                    City = (string)ViewData["City"],
+//                };
+
+//                context.Users.Add(user);
+//                context.SaveChanges();
 
 
-                return View("Success");
-                //return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+//                return View("Success");
+//                //return RedirectToAction("Index");
+//            }
+//            catch
+//            {
+//                return View();
+//            }
+//        }
 
-        // GET: Register/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+//        // GET: Register/Edit/5
+//        public ActionResult Edit(int id)
+//        {
+//            return View();
+//        }
 
-        // POST: Register/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
+//        // POST: Register/Edit/5
+//        [HttpPost]
+//        public ActionResult Edit(int id, FormCollection collection)
+//        {
+//            try
+//            {
+//                // TODO: Add update logic here
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+//                return RedirectToAction("Index");
+//            }
+//            catch
+//            {
+//                return View();
+//            }
+//        }
 
-        // GET: Register/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+//        // GET: Register/Delete/5
+//        public ActionResult Delete(int id)
+//        {
+//            return View();
+//        }
 
-        // POST: Register/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
+//        // POST: Register/Delete/5
+//        [HttpPost]
+//        public ActionResult Delete(int id, FormCollection collection)
+//        {
+//            try
+//            {
+//                // TODO: Add delete logic here
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-    }
-}
+//                return RedirectToAction("Index");
+//            }
+//            catch
+//            {
+//                return View();
+//            }
+//        }
+//    }
+//}
