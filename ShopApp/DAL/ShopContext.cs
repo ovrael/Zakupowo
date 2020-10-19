472efe2842d7762aeeee1c864557f4840638e9ac
Piotrek <piotrek0032@onet.pl>
2020-10-19 21:26:13 +0200

    Adding ShopContext and Shop Initializer on DAL.

---
+++ ShopApp/DAL/ShopContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ShopApp.Models;
+
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ShopApp.DAL
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext() : base("Shop") { }
+        public virtual DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ ShopApp/DAL/ShopInitializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ShopApp.Models;
+using System.Data.Entity;
+
+namespace ShopApp.DAL
+{
+        public class ShopInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<ShopContext>
+        {
+        /*
+            protected override void Seed(ShopContext context)
+            {
+                var Users = new List<User>
+            {
+          
+           * Seed determines which records go every time the base is being initialized
+                    new User{Login="Carson",Password="Alexander",Email="Carson@Alexander.com"},
+                    new User{Login="Alonso",Password="Meredith",Email="Alonso@Meredith.com"},
+                    
+            };
+
+                Users.ForEach(s => context.Users.Add(s));
+                context.SaveChanges();
+             */
+    }
+}
