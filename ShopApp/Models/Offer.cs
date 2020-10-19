8f51a0e76fcd7bc8f77a3245ca8255617956ebf5
ovrael <ovrael@gmail.com>
2020-10-19 21:19:59 +0200

    Updated models classes

--- ShopApp/Models/Offer.cs
+++ ShopApp/Models/Offer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Testowanie.Models
+namespace ShopApp.Models
 {
     public class Offer
     {
--- ShopApp/Models/Product.cs
+++ ShopApp/Models/Product.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
-namespace Testowanie.Models
+namespace ShopApp.Models
 {
     public class Product
     {
