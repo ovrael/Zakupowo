612bd5ba04c7a4e0749e58617f73e3b510503b0b
Kamil <kamciu9@gmail.com>
2020-10-23 23:04:18 +0200

    databse and jquery
    training with database and implementing jquery ajax to the project

---
+++ ShopApp/Controllers/UserPanelController.cs
@@ -0,0 +1,32 @@
+﻿using ShopApp.DAL;
+using ShopApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopApp.Controllers
+{
+    public class UserPanelController : Controller
+    {
+        private ShopContext db = new ShopContext();
+        // GET: UserPanel
+        public ActionResult Index()
+        {
+            ViewBag.Message = db.Users.ToList();
+            return View();
+        }
+        public ActionResult UserPanel()
+        {
+            ViewBag.Message = "Page to manage your profile.";
+
+            return View();
+        }
+        
+
+
+
+    }
+}
