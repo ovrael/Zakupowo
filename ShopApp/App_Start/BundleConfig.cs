78edff2f5463247ddf36c1aa53d698c28df00b4a
Piotrek <piotrek0032@onet.pl>
2020-11-08 18:11:34 +0100

    BundleConfig fix + project tree logic changes WITH SŁAWEK

--- ShopApp/App_Start/BundleConfig.cs
+++ ShopApp/App_Start/BundleConfig.cs
@@ -11,25 +11,27 @@ namespace ShopApp
             bundles.Add(new ScriptBundle("~/bundles/jquery351").Include(
                         "~/Scripts/jquery-3.5.1.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jquery341").Include(
-                        "~/Scripts/jquery-3.4.1.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
             // Użyj wersji deweloperskiej biblioteki Modernizr do nauki i opracowywania rozwiązań. Następnie, kiedy wszystko będzie
             // gotowe do produkcji, użyj narzędzia do kompilowania ze strony https://modernizr.com, aby wybrać wyłącznie potrzebne testy.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            /* bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
+            // TODO: Ogarnięcie co to jest ten modernizr i jak z tego korzystać( bo to może być fajne narzędzie)
+             */
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/umd/popper.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/css/Style.css"));
+            bundles.Add(new StyleBundle("~/bundles/bootstrapcss").Include(
+                      "~/Content/bootstrap.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/Layout").Include(
+                       "~/Content/Layout.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/UserPanel").Include(
+                      "~/Content/UserPanel.css"));
         }
     }
 }
