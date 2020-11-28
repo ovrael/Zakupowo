8e9ab6fb570c3369a77ff0e4a5aa1cd398ab4574
ovrael <ovrael@gmail.com>
2020-11-28 14:48:17 +0100

    Database security in registration (UserController)

---
+++ ShopApp/Utility/Utilities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopApp.Utility
+{
+    public class Utilities
+    {
+        public static bool CheckMajority(DateTime birthDate)
+        {
+            bool result = false;
+
+            if (DateTime.Now.AddYears(-18).CompareTo(birthDate) == 1)
+                result = true;
+
+            return result;
+        }
+
+        public static bool CheckRegistrationAge(DateTime birthDate)
+        {
+            bool result = false;
+
+            if (DateTime.Now.AddYears(-13).CompareTo(birthDate) == 1)
+                result = true;
+
+            return result;
+        }
+    }
+}
