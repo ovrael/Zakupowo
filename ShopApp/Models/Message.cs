a7469fa191740ec539de638a3f53cd56069c89f6
ovrael <ovrael@gmail.com>
2020-12-14 13:37:16 +0100

    Added messege model (needs revision)

---
+++ ShopApp/Models/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using ShopApp.DAL;
+
+namespace ShopApp.Models
+{
+    public class Message : IConcurrencyAwareEntity
+    {
+        [Key]
+        public int MessageID { get; set; }
+        public User Sender { get; set; }
+        public User Receiver { get; set; }
+
+        [Column("Content", TypeName = "nvarchar")]
+        public string Content { get; set; }
+
+        public byte[] RowVersion { get; set; }
+    }
+}
