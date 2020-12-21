388a60993a88d41f3457227502dd6fd09e6214f1
Perlak99 <perlakslawomir@gmail.com>
2020-12-21 13:35:27 +0100

    order.js showing chosed address and added names for inputs for Peter Kaczkson great backendowiec

---
+++ ShopApp/Scripts/Order.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    var id = $("#address-input").val();
+    $(".address").each(function () {
+        if ($(this).attr("id") == "Address_" + id) {
+            $(this).addClass("d-block");
+            $(this).removeClass("d-none");
+        } else {
+            $(this).addClass("d-none");
+            $(this).removeClass("d-block");
+        }
+    })
+    $("#address-input").change(function () {
+        var id = $("#address-input").val();
+        $(".address").each(function () {
+            if ($(this).attr("id") == "Address_" + id) {
+                $(this).addClass("d-block");
+                $(this).removeClass("d-none");
+            } else {
+                $(this).addClass("d-none");
+                $(this).removeClass("d-block");
+            }
+        })
+    })
+});
