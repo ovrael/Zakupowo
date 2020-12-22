1dacae8926c7e95a28651979aa85222b8826784b
Piotrek <63367682+P3rf3cT12@users.noreply.github.com>
2020-12-22 14:52:01 +0100

    Little cleaning

---
+++ ShopApp/DAL/ConcurrencyAwareEntityConvention.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace ShopApp.DAL
+{
+    public interface IConcurrencyAwareEntity
+    {
+        byte[] RowVersion { get; set; }
+    }
+    public class ConcurrencyAwareEntityConvention : Convention
+    {
+        public ConcurrencyAwareEntityConvention()
+        {
+            this.Types<IConcurrencyAwareEntity>().Configure(
+                config => config.Property(e => e.RowVersion).IsRowVersion()
+                );
+        }
+    }
+}
--- ShopApp/DAL/ShopContext.cs
+++ ShopApp/DAL/ShopContext.cs
@@ -10,19 +10,6 @@ using System.Security.AccessControl;
 
 namespace ShopApp.DAL
 {
-    public interface IConcurrencyAwareEntity
-    {
-        byte[] RowVersion { get; set; }
-    }
-    public class ConcurrencyAwareEntityConvention : Convention
-    {
-        public ConcurrencyAwareEntityConvention()
-        {
-            this.Types<IConcurrencyAwareEntity>().Configure(
-                config => config.Property(e => e.RowVersion).IsRowVersion()
-               );
-        }
-    }
     public class ShopContext : DbContext
     {
         public ShopContext() : base("ZakupowoDatabase") { }
