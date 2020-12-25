91709c51aa41b9c9ac9dcc1ef08db83036bbcec1
ovrael <ovrael@gmail.com>
2020-12-25 17:53:08 +0100

    Fixed creating new admin accounts

--- ShopApp/Migrations/Configuration.cs
+++ ShopApp/Migrations/Configuration.cs
@@ -21,36 +21,37 @@ namespace ShopApp.Migrations
 
         protected override void Seed(ShopContext context)
         {
-            var users = new List<User>()
+            User admin = context.Users.Where(u => u.Login == "Zakupowo").FirstOrDefault();
+
+            if (admin == null)
             {
-                new User()
+                var user = new User()
                 {
                     UserID = 1,
                     Email = "Zakupowo2020@gmail.com",
                     Login = "Zakupowo",
                     EncryptedPassword = Utility.Cryptographing.Encrypt("Zakupowo2020$$$"),
-                    FirstName ="Zakupowo",
-                    LastName="Administration",
+                    FirstName = "Zakupowo",
+                    LastName = "Administration",
                     CreationDate = DateTime.Now,
                     Bucket = new Bucket(),
                     Order = new Order()
-                }
-            };
+                };
 
-            var avatarImages = new List<AvatarImage>()
-            {
-                new AvatarImage()
+                var avatarImage = new AvatarImage()
                 {
+                    AvatarImageID = 1,
                     PathToFile = "../../App_Files/Images/UserAvatars/DefaultAvatar.jpg",
-                    User = users[0]
-                }
-            };
+                    User = user
+                };
 
-            avatarImages.ForEach(a => context.AvatarImages.AddOrUpdate(a));
-            context.SaveChanges();
+                context.AvatarImages.Add(avatarImage);
+                context.SaveChanges();
 
-            users.ForEach(u => context.Users.AddOrUpdate(u));
-            context.SaveChanges();
+                context.Users.Add(user);
+                context.SaveChanges();
+            }
+
 
             var categories = new List<Category>
             {
