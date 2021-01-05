72ff0c647baad5ec16e7f2ee2165615361c76548
ovrael <ovrael@gmail.com>
2021-01-05 22:06:02 +0100

    Working on cropping avatar image

--- ShopApp/Scripts/UserPanelSettingsManager.js
+++ ShopApp/Scripts/UserPanelSettingsManager.js
@@ -137,45 +137,45 @@
         });
     });
 
-    $('#avatarInput').click(function (e) {
-        e.preventDefault();
+    // $('#avatarInput').click(function (e) {
+    //     e.preventDefault();
 
-        var input = document.getElementById("avatarFile");
-        var files = input.files;
-        var fileData = new FormData();
+    //     var input = document.getElementById("avatarFile");
+    //     var files = input.files;
+    //     var fileData = new FormData();
 
-        for (var i = 0; i != files.length; i++) {
-            fileData.append("files", files[i]);
-        }
+    //     for (var i = 0; i != files.length; i++) {
+    //         fileData.append("files", files[i]);
+    //     }
 
-        $.ajax({
-            url: "/UserPanel/EditAvatar",
-            data: fileData,
-            processData: false,
-            contentType: false,
-            type: "POST",
-            success: function (returnData) {
-                alert(returnData);
-            },
-            // error handling
-        });
-    });
+    //     $.ajax({
+    //         url: "/UserPanel/EditAvatar",
+    //         data: fileData,
+    //         processData: false,
+    //         contentType: false,
+    //         type: "POST",
+    //         success: function (returnData) {
+    //             alert(returnData);
+    //         },
+    //         // error handling
+    //     });
+    // });
 
-    $(document).on("change", "#avatarFile", function () {
+    // $(document).on("change", "#avatarFile", function () {
 
-        var files = this.files != null ? this.files : [];
-        if (files.length <= 0 || window.FileReader == null) return;
+    //     var files = this.files != null ? this.files : [];
+    //     if (files.length <= 0 || window.FileReader == null) return;
 
 
-        newAvatar = files[0];
+    //     newAvatar = files[0];
 
-        var reader = new FileReader();
-        reader.readAsDataURL(newAvatar);
+    //     var reader = new FileReader();
+    //     reader.readAsDataURL(newAvatar);
 
-        reader.onloadend = function () {
-            document.getElementsByClassName("output")[0].setAttribute("src", this.result);
-        }
-    });
+    //     reader.onloadend = function () {
+    //         document.getElementsByClassName("output")[0].setAttribute("src", this.result);
+    //     }
+    // });
 
 })()
 
