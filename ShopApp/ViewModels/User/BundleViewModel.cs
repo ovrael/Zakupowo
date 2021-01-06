343ded3d5889e475ac950ffd86d4690d9ce2e19b
Piotrek <63367682+P3rf3cT12@users.noreply.github.com>
2021-01-06 18:42:34 +0100

    Adding bundle view mock-up

---
+++ ShopApp/ViewModels/User/BundleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ShopApp.Models;
+
+namespace ShopApp.ViewModels.User
+{
+    public class BundleViewModel
+    {
+            public Bundle Bundle { get; set; }
+            public IEnumerable<Offer> OffersList { get; set; }
+            public bool IsInBucket { get; set; }
+            public bool IsInFavourite { get; set; }
+    }
+}
