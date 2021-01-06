cf510badf97656b2334632760f2e5ff017d879b1
ovrael <ovrael@gmail.com>
2021-01-06 14:09:48 +0100

    Fixed adresses page in userpanel, added max adresses count - now equals 4

--- ShopApp/Scripts/UserPanelSettingsManager.js
+++ ShopApp/Scripts/UserPanelSettingsManager.js
@@ -136,47 +136,6 @@
             // error handling
         });
     });
-
-    // $('#avatarInput').click(function (e) {
-    //     e.preventDefault();
-
-    //     var input = document.getElementById("avatarFile");
-    //     var files = input.files;
-    //     var fileData = new FormData();
-
-    //     for (var i = 0; i != files.length; i++) {
-    //         fileData.append("files", files[i]);
-    //     }
-
-    //     $.ajax({
-    //         url: "/UserPanel/EditAvatar",
-    //         data: fileData,
-    //         processData: false,
-    //         contentType: false,
-    //         type: "POST",
-    //         success: function (returnData) {
-    //             alert(returnData);
-    //         },
-    //         // error handling
-    //     });
-    // });
-
-    // $(document).on("change", "#avatarFile", function () {
-
-    //     var files = this.files != null ? this.files : [];
-    //     if (files.length <= 0 || window.FileReader == null) return;
-
-
-    //     newAvatar = files[0];
-
-    //     var reader = new FileReader();
-    //     reader.readAsDataURL(newAvatar);
-
-    //     reader.onloadend = function () {
-    //         document.getElementsByClassName("output")[0].setAttribute("src", this.result);
-    //     }
-    // });
-
 })()
 
 function createAdressDiv(adress, number) {
