ba7e1de445433aeef97fc127fdf038fd01c7ca3c
Perlak99 <perlakslawomir@gmail.com>
2021-01-17 17:18:24 +0100

    doesn't matter

--- ShopApp/Scripts/login.js
+++ ShopApp/Scripts/login.js
@@ -4,14 +4,12 @@
             error.appendTo($("#" + element.attr('id') + "_validation"));
         },
         rules: {
-            emailAddressInput: "required",
             passwordInput: {
                 required: true,
                 minlength: 8
             },
         },
         messages: {
-            emailAddressInput: "Podaj swój e-mail",
             passwordInput: {
                 required: "Podaj swoje hasło",
                 minlength: "Hasło musi posiadać co najmniej 8 znaków"
