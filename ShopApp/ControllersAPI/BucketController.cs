9d478acf5543ae7efbeb53d8798c0b3e46115008
kamreo <kamciu9@gmail.com>
2021-01-18 03:46:04 +0100

    api update

--- ShopApp/ControllersAPI/BucketController.cs
+++ ShopApp/ControllersAPI/BucketController.cs
@@ -1,30 +1,38 @@
-﻿using ShopApp.DAL;
+﻿using Newtonsoft.Json;
+using ShopApp.DAL;
 using ShopApp.Models;
+using ShopApp.Utility;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 
+
 namespace ShopApp.ControllersAPI
 {
+    [System.Web.Http.RoutePrefix("api/Bucket")]
     public class BucketController : ApiController
     {
         private ShopContext db = new ShopContext();
         // GET: Default
-        public IHttpActionResult GetOffersByCategory(int id)
+        public IHttpActionResult GetBucketItems(int id)
         {
             var user = db.Users.Where(i => i.UserID == id).FirstOrDefault();
             if (user != null)
             {
+                
                 var BucketItems = user.Bucket.BucketItems;
 
                 List<BindingBucketItem> list = new List<BindingBucketItem>();
-                foreach(var item in BucketItems)
+                foreach (var item in BucketItems)
                 {
                     list.Add(BindingBucketItem.Convert(item));
                 }
+        
                 //Consider using Critical error page for below
                 if (user.ShippingAdresses.Count() == 0)
                     return Ok("Przed przejściem do kasy wymagane jest ustawienie adresu dostawy");
@@ -40,32 +48,140 @@ namespace ShopApp.ControllersAPI
 
         }
 
-    }
 
-    public class BindingBucketItem 
-    {
-        public int BucketItemID { get; set; }
-        public int Quantity { get; set; }
-        public double TotalPrice { get; set; }
-        public bool IsChosen { get; set; }
-        public virtual OfferItem Offer { get; set; }
 
-        public static BindingBucketItem Convert(BucketItem bucketItem)
-        {
-            OfferItem offer = OfferItem.ConvertOfferToOfferItem(bucketItem.Offer);
-            BindingBucketItem item = new BindingBucketItem
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("BuyBucketItems")]
+        public async Task<IHttpActionResult> BuyBucketItemsAsync()
+        { 
+            string json = await Request.Content.ReadAsStringAsync();
+            dynamic data = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+            string login = data["login"];
+            int addressId = Convert.ToInt32(data["addressId"]);
+
+            var user = db.Users.Where(i => i.Login == login && i.IsActivated).FirstOrDefault();
+            //Odpowiednie errorr message dlaczego
+            if (user != null)
             {
-                BucketItemID = bucketItem.BucketItemID,
-                Quantity = bucketItem.Quantity,
-                TotalPrice = bucketItem.TotalPrice,
-                IsChosen = bucketItem.IsChosen,
-                Offer = offer
-            };
-            return item;
+                var order = user.Order?.BucketItems?.ToList();
+                var Address = user.ShippingAdresses?.Where(i => i.AdressID == addressId).FirstOrDefault();
+                List<BucketItem> ItemsThatCouldntBeenSold = new List<BucketItem>();
+                if (order != null && Address != null)
+                {
+                    var grouped = order.GroupBy(i => i.Offer != null ? i.Offer.User : i.Bundle.User);
+                    foreach (var seller in grouped)
+                    {
+                        var message = "Jestem zainteresowany zakupem wystawionego produktu proszę o odpowiedź.";
+                        
+                        if (!EmailManager.SendEmail(EmailManager.EmailType.TransactionRequest, seller.Key.FirstName, seller.Key.LastName, seller.Key.Email, user.Login, user.FirstName, user.LastName, seller.ToList(), message, Address))
+                            //check that
+                            seller.ToList().ForEach(i => ItemsThatCouldntBeenSold.Add(i));
+                        else
+                        {
+                            Transaction transaction = new Transaction()
+                            {
+                                Buyer = user,
+                                Seller = seller.Key,
+                                BucketItems = seller.ToList(), 
+                                                               
+                                CreationDate = DateTime.UtcNow,
+                                IsAccepted = false,
+                                IsChosen = false
+                            };
+
+                            db.Transactions.Add(transaction);
+                            db.SaveChanges();
+
+                            user.BoughtTransactions.Add(transaction);
+                            db.SaveChanges();
+
+                            seller.Key.SoldTransactions.Add(transaction);
+                            db.SaveChanges();
+
+                          
+
+                        }
+                    }
+                    if (ItemsThatCouldntBeenSold != null && ItemsThatCouldntBeenSold.Count() != 0)
+                    {
+                     
+                    }
+                    var BucketItemsInOrderTab = user.Order.BucketItems.ToArray();
+
+                    for (int x = 0; x < BucketItemsInOrderTab.Count(); x++)
+                    {
+                        var item = BucketItemsInOrderTab.ElementAt(x);
+                        if (ItemsThatCouldntBeenSold != null && ItemsThatCouldntBeenSold.Count() != 0 && ItemsThatCouldntBeenSold.Contains(item))
+                            continue;
+                        else
+                        {
+                            try
+                            {
+                                user.Bucket.BucketItems.Remove(item);
+                                db.SaveChanges();
+
+                            }
+                            catch (Exception ex)
+                            {
+                             
+                            }
+                        }
+                    }
+
+                    ICollection<BucketItem> list = null;
+
+                    try
+                    {
+                       list = user.Bucket.BucketItems;
+                    }
+                    catch(Exception e)
+                    {
+                        Debug.WriteLine(e.Message);
+                    }
+                  
+                    foreach (var item in list)
+                    {
+                        db.BucketItems.Remove(item);
+                        db.SaveChanges();
+
+                    }
+                   
+                    user.Order.BucketItems.Clear();
+                    db.SaveChanges();
+                    ConcurencyHandling.SaveChangesWithConcurencyHandling(db);
+                    return Ok();
+                }
+            }
+            return BadRequest();
         }
 
+
+
+        public class BindingBucketItem
+        {
+            public int BucketItemID { get; set; }
+            public int Quantity { get; set; }
+            public double TotalPrice { get; set; }
+            public bool IsChosen { get; set; }
+            public virtual OfferItem Offer { get; set; }
+
+            public static BindingBucketItem Convert(BucketItem bucketItem)
+            {
+                OfferItem offer = OfferItem.ConvertOfferToOfferItem(bucketItem.Offer);
+                BindingBucketItem item = new BindingBucketItem
+                {
+                    BucketItemID = bucketItem.BucketItemID,
+                    Quantity = bucketItem.Quantity,
+                    TotalPrice = bucketItem.TotalPrice,
+                    IsChosen = bucketItem.IsChosen,
+                    Offer = offer
+                };
+                return item;
+            }
+
+        }
+
+     
+
     }
-
-   
-
 }
