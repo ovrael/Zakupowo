2cfef66353d109f6072273f1c35d3a1e92dd4f49
kamreo <kamciu9@gmail.com>
2021-01-19 01:40:09 +0100

    api updates

--- ShopApp/ControllersAPI/BucketController.cs
+++ ShopApp/ControllersAPI/BucketController.cs
@@ -53,17 +53,20 @@ namespace ShopApp.ControllersAPI
         [System.Web.Http.HttpPost]
         [System.Web.Http.Route("BuyBucketItems")]
         public async Task<IHttpActionResult> BuyBucketItemsAsync()
-        { 
+        {  
             string json = await Request.Content.ReadAsStringAsync();
             dynamic data = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
             string login = data["login"];
             int addressId = Convert.ToInt32(data["addressId"]);
 
+         
             var user = db.Users.Where(i => i.Login == login && i.IsActivated).FirstOrDefault();
+            
             //Odpowiednie errorr message dlaczego
             if (user != null)
             {
-                var order = user.Order?.BucketItems?.ToList();
+                
+                var order = user.Bucket?.BucketItems?.ToList();
                 var Address = user.ShippingAdresses?.Where(i => i.AdressID == addressId).FirstOrDefault();
                 List<BucketItem> ItemsThatCouldntBeenSold = new List<BucketItem>();
                 if (order != null && Address != null)
@@ -82,8 +85,8 @@ namespace ShopApp.ControllersAPI
                             {
                                 Buyer = user,
                                 Seller = seller.Key,
-                                BucketItems = seller.ToList(), 
-                                                               
+                                BucketItems = seller.ToList(), // NIE PRZYPISUJE ALE NIE WYWALA BŁĘDU??
+                                                               //BucketItems = (ICollection<BucketItem>)seller, // DOBRZE PRZYPISUJE, ALE WYWALA BŁĄD ŻĄDANIA
                                 CreationDate = DateTime.UtcNow,
                                 IsAccepted = false,
                                 IsChosen = false
@@ -97,17 +100,31 @@ namespace ShopApp.ControllersAPI
 
                             seller.Key.SoldTransactions.Add(transaction);
                             db.SaveChanges();
+                            //ConcurencyHandling.SaveChangesWithConcurencyHandling(db);
 
-                          
+                            //foreach (var item in seller)
+                            //{
+                            //    item.Transaction.Add(transaction);
+                            //    ConcurencyHandling.SaveChangesWithConcurencyHandling(db);
+                            //}
 
+                            //ConcurencyHandling.SaveChangesWithConcurencyHandling(db);
+                            //WyswietlanieTransakcji(transaction);
+
+                            //foreach (var offer in seller.ToList())
+                            //{
+                            //    offer.Transaction.Add(transaction);
+                            //}
+                            //ConcurencyHandling.SaveChangesWithConcurencyHandling(db);
+                            //foreach (var offer in seller.ToList())
+                            //{
+                            //    offer.Transaction.Add(transaction);
+                            //}
+                            //ConcurencyHandling.SaveChangesWithConcurencyHandling(db);
                         }
                     }
-                    if (ItemsThatCouldntBeenSold != null && ItemsThatCouldntBeenSold.Count() != 0)
-                    {
-                     
-                    }
-                    var BucketItemsInOrderTab = user.Order.BucketItems.ToArray();
-
+                   
+                    var BucketItemsInOrderTab = user.Bucket.BucketItems.ToArray();
                     for (int x = 0; x < BucketItemsInOrderTab.Count(); x++)
                     {
                         var item = BucketItemsInOrderTab.ElementAt(x);
@@ -120,40 +137,72 @@ namespace ShopApp.ControllersAPI
                                 user.Bucket.BucketItems.Remove(item);
                                 db.SaveChanges();
 
+                                //Offer itemOffer = item.Offer;
+                                //itemOffer.BucketItems.Remove(item);
+                                //db.SaveChanges();
+
+                                //db.Entry(item).State = System.Data.Entity.EntityState.Deleted;
+                                //db.SaveChanges();
+
                             }
                             catch (Exception ex)
                             {
-                             
+                                Debug.WriteLine("--------------------- ERROR: " + ex.InnerException.Message);
                             }
+
+                            //db.BucketItems.Remove(item);
+                            //await RemoveFromBucket(item.Offer != null ? "Offer" : "Bundle", item.Offer != null ? item.Offer.OfferID : item.Bundle.BundleID);
                         }
                     }
 
-                    ICollection<BucketItem> list = null;
-
-                    try
-                    {
-                       list = user.Bucket.BucketItems;
-                    }
-                    catch(Exception e)
-                    {
-                        Debug.WriteLine(e.Message);
-                    }
-                  
-                    foreach (var item in list)
-                    {
-                        db.BucketItems.Remove(item);
-                        db.SaveChanges();
-
-                    }
-                   
                     user.Order.BucketItems.Clear();
-                    db.SaveChanges();
                     ConcurencyHandling.SaveChangesWithConcurencyHandling(db);
-                    return Ok();
                 }
             }
             return BadRequest();
         }
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("RemoveFromBucket")]
+        public async Task<IHttpActionResult> RemoveFromBucket()
+        {
+
+            string json = await Request.Content.ReadAsStringAsync();
+            dynamic data = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+            string login = data["login"];
+            int id = Convert.ToInt32(data["offerId"]);
+
+
+            var user = db.Users.Where(i => i.Login == login && i.IsActivated).FirstOrDefault();
+          
+
+            if ( user != null && id > 0)
+            {
+                
+                    var Offer = db.Offers.Where(i => i.OfferID == id).FirstOrDefault();
+                    if (Offer != null)
+                    {
+                        BucketItem BucketItemToRemove = user.Bucket?.BucketItems?.Where(i => i.Offer != null && i.Offer.OfferID == id).FirstOrDefault();
+                        if (BucketItemToRemove != null)//We chceck if user called for existing and active offer
+                        {
+                            user.Bucket.BucketItems.Remove(BucketItemToRemove);
+                            Offer.BucketItems.Remove(BucketItemToRemove);
+                            db.BucketItems.Remove(BucketItemToRemove);
+                            ConcurencyHandling.SaveChangesWithConcurencyHandling(db);
+                        }
+                        else
+                            return Ok( "Nie posiadasz tej oferty w swoim koszyku");
+                    }
+                    else
+                    return Ok("Wybrana oferta nie istnieje");
+
+            }
+            else
+            return BadRequest("Wprowadzone dane są niepoprawne");
+
+            return BadRequest();
+        }
+
 
 
 
